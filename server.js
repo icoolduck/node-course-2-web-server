5f60ca3e6e14bce19f1238643cb1e45f96a336b6
kartung <kartung@gmail.com>
2018-05-28 09:47:38 +0800

    Added a project page

--- server.js
+++ server.js
@@ -50,6 +50,13 @@ app.get('/about', (req, res) => {
   });
 });
 
+
+app.get('/project', (req, res) => {
+  res.render('project.hbs', {
+    pageTitle: 'The Awesome Project Page'
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage : 'Unable to handle request'});
